433506cbdefb9ebcc48286ab80b9c9d0af962c03
Brian C. Mills <briancmills@users.noreply.github.com>
2021-12-07 20:45:26 -0500

    Minor variable name change

--- Java-Stuff/HelloUser/code/src/Main.java
+++ Java-Stuff/HelloUser/code/src/Main.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-    // this is a comment
+    
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("What is your name?");
-        String name = myObj.nextLine();
+        String name = scanner.nextLine();
         System.out.println("Hello " + name + ", you are great!");
     }
 }
