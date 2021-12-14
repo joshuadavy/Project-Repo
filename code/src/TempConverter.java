53df608c042753273f3c94106f4de89348acd07b
joshuadavy <91229371+joshuadavy@users.noreply.github.com>
2021-12-14 18:52:16 -0500

    lets see what happens

--- code/src/TempConverter.java
+++ code/src/TempConverter.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class TempConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a Temperature");
+        System.out.println("Enter a Temperature:");
         int tempNum = Integer.parseInt(scanner.nextLine());
         int tempConv = ((tempNum - 32) * 5 / 9);
         System.out.println("Was the temperature in fahrenheit or celsius? Please Enter 'C' or 'F'");
         String tempKind = scanner.nextLine();
         if (tempKind.equals("F")){
-            System.out.println("The temperature in celsius is " + tempConv);
+            System.out.println(tempNum + tempKind + " is converted to " + tempConv + "C");
         }
         else {
-            System.out.println("The temperature in celsius is " + tempNum);
+            System.out.println("The temperature is " + tempNum + "C");
         }
     }
 }
