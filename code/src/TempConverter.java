bbd39bfafb7062c7881006fdcbc94781e8f92fe7
joshuadavy <91229371+joshuadavy@users.noreply.github.com>
2022-01-03 18:51:48 -0500

    I added conversion from C to F as well as adding formatting to round to two places. This does not include code that can interpret input such as "25F" and convert it to C without asking for the system of measure

--- code/src/TempConverter.java
+++ code/src/TempConverter.java
@@ -1,22 +1,59 @@
 import java.util.Scanner;
 
+
+/**
+ * @author joshdavy
+ * A simple main method that supports converting temperatures between fahrenheit and celsius.
+ */
 public class TempConverter {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a Temperature:");
-        int tempNum = Integer.parseInt(scanner.nextLine());
-        do {
+        System.out.print("Enter a Temperature: ");
+        String numberInput = scanner.nextLine().trim();
 
-        };
-        while ();
-        int tempConv = ((tempNum - 32) * 5 / 9);
-        System.out.println("Was the temperature in fahrenheit or celsius? Please Enter 'C' or 'F'");
-        String tempKind = scanner.nextLine();
-        if (tempKind.equals("F")){
-            System.out.println(tempNum + tempKind + " is converted to " + tempConv + "C");
+        if (!isNumeric(numberInput)) {
+            System.err.println("\nInput was not a number: " + numberInput);
+            System.exit(1);
+        }
+
+        double fromTemperature = Double.parseDouble(numberInput);
+
+        System.out.print("Was the temperature in fahrenheit or celsius? (Please Enter 'C' or 'F')? ");
+        String systemOfMeasurement = scanner.nextLine();
+
+        double celsiusConvertedTemperature = ((fromTemperature * 9/5)+ 32);
+        double fahrenheitConvertedTemperature = ((fromTemperature - 32) * 5 / 9);
+
+        if (systemOfMeasurement.equalsIgnoreCase("F")){
+            System.out.print(fromTemperature + systemOfMeasurement);
+            System.out.printf( " is converted to %.2f" , fahrenheitConvertedTemperature);
+            System.out.print("C");
+
         }
         else {
-            System.out.println("The temperature is " + tempNum + "C");
+            System.out.print(fromTemperature + systemOfMeasurement);
+            System.out.printf( " is converted to %.2f" , celsiusConvertedTemperature);
+            System.out.print("F");
+
         }
+    }
+
+    /**
+     *
+     * @param input The input string to check.
+     * @return A boolean value that indicates whether the provided string input can safely be converted to a Double.
+     */
+    public static boolean isNumeric(String input) {
+        if (input == null) {
+            return false;
+        }
+
+        try {
+             Double.parseDouble(input.trim());
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
