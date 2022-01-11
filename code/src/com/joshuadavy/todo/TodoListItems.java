bbfc43f00c1299842ba6cc418e7e1d158578d00b
joshuadavy <91229371+joshuadavy@users.noreply.github.com>
2022-01-10 19:21:49 -0500

    Two class files that are the start of a commandline Todo List, proposed features are: to accept multiple todo items, and then declare their urgency

---
+++ code/src/com/joshuadavy/todo/TodoListItems.java
@@ -0,0 +1,11 @@
+package com.joshuadavy.todo;
+/**
+ * @author joshdavy
+ * The goal is to create and format the inputs into an array by using this class, so that
+ * the main method simply outputs the formatted array
+ */
+public class TodoListItems {
+    String [] todoListItemArray;
+
+}
+
