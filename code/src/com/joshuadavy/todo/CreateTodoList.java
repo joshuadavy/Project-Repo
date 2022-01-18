bcf26a4f7cc110b3fb7659bf3cbd23053c7fee2c
joshuadavy <91229371+joshuadavy@users.noreply.github.com>
2022-01-18 18:45:57 -0500

    I added some validation and a few lines to test if mark() was functional

--- code/src/com/joshuadavy/todo/CreateTodoList.java
+++ code/src/com/joshuadavy/todo/CreateTodoList.java
@@ -15,5 +15,22 @@ public class CreateTodoList {
         System.out.print("Please enter a description for the todo item: ");
         item1.setDescription(scanner.nextLine().trim());
         System.out.println(item1.getDescription());
+        System.out.print("Has " + item1.getDescription() + " been completed? : ");
+        String input = scanner.nextLine().trim();
+        item1.mark(isMarked(input));
+    }
+
+    public static boolean isMarked(String input){
+        if (input == null){
+            return false;
+        }
+
+        if (input.equals("yes")){
+            return true;
+        }
+
+        else return false;
+
     }
 }
+
